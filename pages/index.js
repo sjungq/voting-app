6ea239d0da40d081c264dcb6af2af9f69e8d6e8d
Stacy Jung <stacyj1417@gmail.com>
2021-11-06 15:03:29 -0700

    Testing Express server+Next frontend on Heroku setup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-const Index = () => {
+import axios from 'axios';
+const Index = ({ data }) => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <p>Test</p>
+      <p>{data.id}</p>
+      <p>{data.text}</p>
     </div>
   );
+};
+
+Index.getInitialProps = async () => {
+  const res = await axios.get('http://localhost:4000/api/test');
+  return { data: res.data };
 };
 
 export default Index;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 //External imports
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 //express middleware
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+app.get('/api/test', (req, res) => {
+  const testJson = { id: 0, text: 'Monica' };
+  res.json(testJson);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
