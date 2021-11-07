7b4b16847547fa99c5f38d2e5a1f641a52ae4661
Stacy Jung <stacyj1417@gmail.com>
2021-11-06 22:49:08 -0700

    Display polls from DB; POC before restructuring project

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,26 @@ import axios from 'axios';
 const Index = ({ polls }) => {
   return (
     <div>
-      <p>Test</p>
-      <ul></ul>
+      <div>
+        {polls.map((poll) => (
+          <ul key={poll._id}>
+            <h3>{poll.pollPrompt}</h3>
+            {poll.pollOptions.map((option) => (
+              <li key={option._id}>
+                <span>{option.optionText}</span> -
+                <span> {option.voteCount} votes</span>
+              </li>
+            ))}
+          </ul>
+        ))}
+      </div>
     </div>
   );
 };
 
 Index.getInitialProps = async () => {
   const res = await axios.get(`https://voting-app-sj.herokuapp.com/api/test`);
-  const { data } = res;
+  const { data } = res.data;
   return { polls: data };
 };
 
